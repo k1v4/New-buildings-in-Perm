c43c822682e047a5ee185ac4930485c92b192c89
k1v4 <chvik1@yandex.ru>
2024-03-02 00:48:46 +0500

    1) Сделана HTML основа слайдера 2) Начата скрипт для работы данного слайдера

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-$(document).ready(function () {
-  $('.items').slick({
-    infinite: true,
-    lazyLoad: 'ondemand',
-    slidesToShow: 3,
-    slidesToScroll: 3,
-  });
+const photos = [
+  'images/slider1_photo1.png',
+  'images/slider1_photo2.png',
+  'images/slider1_photo3.png',
+];
+const len = photos.length;
+let currentPhotoIndex1 = 0;
+let currentPhotoIndex2 = 1;
+let currentPhotoIndex3 = 2;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const imageElementLeft = document.getElementById('left-arrow');
+  const imageElementRight = document.getElementById('right-arrow');
+
+  imageElementLeft.onclick = function () {
+    currentPhotoIndex1++;
+    currentPhotoIndex2++;
+    currentPhotoIndex3++;
+
+    if (currentPhotoIndex3 > len - 1) {
+      currentPhotoIndex3 = 0;
+    }
+
+    alert(
+      currentPhotoIndex1 + ' ' + currentPhotoIndex2 + ' ' + currentPhotoIndex3
+    );
+  };
+
+  imageElementRight.onclick = function () {
+    currentPhotoIndex1--;
+    currentPhotoIndex2--;
+    currentPhotoIndex3--;
+
+    if (currentPhotoIndex3 < 0) {
+      currentPhotoIndex3 = len - 1;
+    }
+
+    alert(
+      currentPhotoIndex1 + ' ' + currentPhotoIndex2 + ' ' + currentPhotoIndex3
+    );
+  };
 });
