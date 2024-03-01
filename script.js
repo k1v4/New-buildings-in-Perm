ab4104edc8cfd74094dc00bcf4bd6950370c1116
k1v4 <chvik1@yandex.ru>
2024-03-02 01:03:36 +0500

    Исправлено изменение индексов в первом слайдере

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ let currentPhotoIndex2 = 1;
 let currentPhotoIndex3 = 2;
 
 document.addEventListener('DOMContentLoaded', function () {
+  cd;
   const imageElementLeft = document.getElementById('left-arrow');
   const imageElementRight = document.getElementById('right-arrow');
 
   imageElementLeft.onclick = function () {
-    currentPhotoIndex1++;
-    currentPhotoIndex2++;
+    currentPhotoIndex1 = currentPhotoIndex2;
+    currentPhotoIndex2 = currentPhotoIndex3;
     currentPhotoIndex3++;
 
     if (currentPhotoIndex3 > len - 1) {
@@ -27,12 +28,12 @@ document.addEventListener('DOMContentLoaded', function () {
   };
 
   imageElementRight.onclick = function () {
+    currentPhotoIndex3 = currentPhotoIndex2;
+    currentPhotoIndex2 = currentPhotoIndex1;
     currentPhotoIndex1--;
-    currentPhotoIndex2--;
-    currentPhotoIndex3--;
 
-    if (currentPhotoIndex3 < 0) {
-      currentPhotoIndex3 = len - 1;
+    if (currentPhotoIndex1 < 0) {
+      currentPhotoIndex1 = len - 1;
     }
 
     alert(
